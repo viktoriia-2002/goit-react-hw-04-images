8528352551fad6b56d41baa4b9804f1fc3066255
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-08-01 11:55:42 -0400

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,86 +1,3 @@
-// import React from 'react';
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import ImageGallery from './ImageGallery';
-// import Searchbar from './Searchbar';
-// import ButtonLoadMore from './Button/Button';
-// import Modal from './Modal/Modal';
-
-// class App extends React.Component {
-//   state = {
-//     searchImage: '',
-//     page: 0,
-//     loading: false,
-//     imageCards: [],
-//     isOpenModal: false,
-//     selectedPicture: null,
-//   };
-
-//   handleImages = (array, page) => {
-//     this.setState({ imageCards: array });
-//   };
-
-//   handleLoading = loading => {
-//     this.setState({ loading });
-//   };
-
-//   handleSearchSubmit = searchImage => {
-//     this.setState({ searchImage });
-//   };
-
-//   handleLoadMore = () => {
-//     this.setState(prevState => ({ page: prevState.page + 1 }));
-//   };
-
-//   handleOpenModal = id => {
-//     console.log({ id });
-//     this.setState({ isOpenModal: true });
-//     const selectedPicture = this.state.imageCards.find(
-//       image => image.id === id
-//     )?.largeImageURL;
-//     if (selectedPicture) {
-//       this.setState({ selectedPicture });
-//     }
-//   };
-
-//   handleCloseModal = () => {
-//     this.setState({ isOpenModal: false });
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <Searchbar onSubmit={this.handleSearchSubmit} />
-//         <ImageGallery
-//           searchImage={this.state.searchImage}
-//           page={this.state.page}
-//           handleImages={this.handleImages}
-//           imageCards={this.state.imageCards}
-//           handleLoading={this.handleLoading}
-//           loading={this.state.loading}
-//           handleOpenModal={this.handleOpenModal}
-//         />
-
-//         <Modal
-//           isOpenModal={this.state.isOpenModal}
-//           selectedPicture={this.state.selectedPicture}
-//           handleCloseModal={this.handleCloseModal}
-//         />
-
-//         <ButtonLoadMore
-//           handleLoadMore={this.handleLoadMore}
-//           page={this.state.page}
-//           imageCards={this.state.imageCards}
-//           loading={this.state.loading}
-//         />
-//         <ToastContainer autoClose={1000} />
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
-
 import { useState } from 'react';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
@@ -127,7 +44,6 @@ const App = () => {
   const handleCloseModal = () => {
     setIsOpenModal(false);
   };
-
 
   return (
     <>
