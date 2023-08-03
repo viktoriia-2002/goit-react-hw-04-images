58a237791f14ebc37523955583df1ca53ce7d5c9
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-08-02 22:15:15 -0400

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import React from 'react';
+import { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ImageGallery from './ImageGallery';
 import Searchbar from './Searchbar';
 import ButtonLoadMore from './Button/Button';
 import Modal from './Modal/Modal';
+
+const apiUrl = 'https://pixabay.com/api/';
+const apiKey = '36126930-7b2057d774b58ed23a3e8d721';
 
 const App = () => {
   const [searchImage, setSearchImage] = useState('');
@@ -15,17 +19,39 @@ const App = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [selectedPicture, setSelectedPicture] = useState(null);
 
-  const handleImages = (array, page) => {
-    setImageCards(array);
-  };
-
   const handleLoading = isLoading => {
     setLoading(isLoading);
   };
 
-  const handleSearchSubmit = searchImage => {
-    setSearchImage(searchImage);
+  const handleSearchSubmit = image => {
+    setSearchImage(image);
+    if (searchImage !== image) {
+      setPage(0);
+    }
   };
+
+  useEffect(() => {
+    if (searchImage.length) {
+      handleLoading(true);
+
+      setTimeout(() => {
+        fetch(
+          `${apiUrl}?q=${searchImage}&page=${
+            page === 0 ? 1 : page + 1
+          }&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`
+        )
+          .then(response => response.json())
+          .then(imageCards => {
+            if (imageCards.hits.length) {
+              setImageCards(prevImages =>
+                page > 0 ? [...prevImages, ...imageCards.hits] : imageCards.hits
+              );
+            }
+          })
+          .finally(handleLoading(false));
+      }, 2000);
+    }
+  }, [page, searchImage]);
 
   const handleLoadMore = () => {
     setPage(prevPage => prevPage + 1);
@@ -49,11 +75,7 @@ const App = () => {
     <>
       <Searchbar onSubmit={handleSearchSubmit} />
       <ImageGallery
-        searchImage={searchImage}
-        page={page}
-        handleImages={handleImages}
         imageCards={imageCards}
-        handleLoading={handleLoading}
         loading={loading}
         handleOpenModal={handleOpenModal}
       />
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,64 +1,8 @@
-import { useEffect } from 'react';
 import ImageGalleryItem from 'components/ImageGalleryItem';
 import { ImagesList } from './ImageGallery.styled';
 import Loader from '../Loader';
-// import { useState } from 'react';
 
-// const apiUrl = 'https://pixabay.com/api/';
-// const apiKey = '36126930-7b2057d774b58ed23a3e8d721';
-
-const ImageGallery = ({
-  imageCards,
-  loading,
-  handleOpenModal,
-  searchImage,
-  page,
-  handleImages,
-  handleLoading,
-}) => {
-  // const [prevImages, setPrevImages] = useState([]);
-  // const [prevPage, setPrevsPage] = useState('');
-
-  // useEffect(() => {
-  //   if (searchImage.length) {
-  //     handleLoading(true);
-  //     const isNewSearch = prevPage === page && imageCards.length !== 0;
-  //     setTimeout(() => {
-  //       fetch(
-  //         `${apiUrl}?q=${searchImage}&page=${
-  //           page === 0 ? 1 : page + 1
-  //         }&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`
-  //       )
-  //         .then(response => response.json())
-  //         .then(imageCards => {
-  //           if (imageCards.hits.length > 0) {
-  //             const images = isNewSearch
-  //               ? [...imageCards.hits]
-  //               : [...prevImages, ...imageCards.hits];
-
-  //             handleImages(images);
-  //             setPrevImages(images);
-  //             setPrevsPage(page);
-  //           }
-  //         })
-  //         .finally(handleLoading(false));
-  //     }, 2000);
-  //   }
-  // }, [
-  //   searchImage,
-  //   page,
-  //   handleImages,
-  //   handleLoading,
-  //   imageCards,
-  //   prevImages,
-  //   prevPage,
-  // ]);
-
-  useEffect(() => {
-    console.log('Test');
-  }, [searchImage, page]);
-  // }, [searchImage, page]); Будь ласка розкоментуйте }, [searchImage, page]) і залішить searchImage, page в dependeies i перевірьте мій код
-
+const ImageGallery = ({ imageCards, loading, handleOpenModal }) => {
   return (
     <div>
       {loading && <Loader />}
